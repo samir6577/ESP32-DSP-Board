1df1317c7664fb5fdd8ffbdadd037db666ac1803
Samir Gulamhussein <samir6577@outlook.com>
2026-04-12 12:47:33 +0100

    Input filter altered, output amplifier fixed, 22uf decoupling added to charge pump negative output

--- Firmware/DSP Board Firmware/DSP_Firmware/DSP_Firmware.ino
+++ Firmware/DSP Board Firmware/DSP_Firmware/DSP_Firmware.ino
@@ -1,25 +1,28 @@
 #include <TLC7524CN.h>
 #include <MCP3XXX.h>
+//control pins for amplifier
+const int A = 14;
+const int B = 13;
 // pins for SPI ADC
-uint8_t miso = 14;
-uint8_t mosi = 12;
-uint8_t cs = 13;
-uint8_t clk = 27;
-// pins for DAC
+uint8_t miso = 21;
+uint8_t mosi = 47;
+uint8_t cs = 48;
+uint8_t clk = 45;
 const int freq = 10000;
 const int resolution = 8;
 const int pwm_channel = 0;
 //const int adc_input_pin = 13;
-const int dac_wr = 18;
-const int dac_cs = 15;
-const int dac_d0 = 19;
-const int dac_d1 = 21;
-const int dac_d2 = 22;
-const int dac_d3 = 23;
-const int dac_d4 = 4;
-const int dac_d5 = 5;
-const int dac_d6 = 16;
-const int dac_d7 = 17;
+// pins for DAC
+const int dac_wr = 46;
+const int dac_cs = 3;
+const int dac_d0 = 18;
+const int dac_d1 = 17;
+const int dac_d2 = 16;
+const int dac_d3 = 4;
+const int dac_d4 = 5;
+const int dac_d5 = 6;
+const int dac_d6 = 7;
+const int dac_d7 = 15;
 // number of coefficients
 const int taps = 17;
 // array for stored signal samples initially 0 
@@ -30,23 +33,23 @@ float input_x_coefficients[taps];
 int newest_val = 0;
 // array for FIR Filter Coefficients
 float filter_coefficients[taps] = {
-0.000811, 
-0.003570, 
-0.008920, 
-0.017198, 
-0.027937, 
-0.039756, 
-0.050594, 
-0.058241, 
-0.061002, 
-0.058241, 
-0.050594, 
-0.039756, 
-0.027937, 
-0.017198, 
-0.008920, 
-0.003570, 
-0.000811
+0.000696, 
+0.003527, 
+0.009773, 
+0.020360, 
+0.035047, 
+0.052023, 
+0.068138, 
+0.079762, 
+0.084004, 
+0.079762, 
+0.068138, 
+0.052023, 
+0.035047, 
+0.020360, 
+0.009773, 
+0.003527, 
+0.000696
 };
 // variable to store output
 float output_val = 0;
@@ -57,12 +60,20 @@ MCP3004 adc;
 
 void setup() {
   // put your setup code here, to run once:
-  analogSetWidth(9);
+  pinMode(8,OUTPUT);
+  digitalWrite(8, HIGH);
+  // setup amplifier control pins
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
   // set array of samples to zero initially
   for(int i = 0; i < taps; i++){
     input_samples[i] = 0;
   }
   Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
   adc.begin(cs,mosi,miso,clk);
 }
 
@@ -73,11 +84,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   // store latest sampled value from adc
   //t1 = micros();
-  newest_val = adc.analogRead(0);
+  newest_val = adc.analogRead(2);
   // DAC is 8 bits so limit adc input to 255
   if(newest_val > 255){
     newest_val = 255; 
   }
+  //newest_val = newest_val & 0011111111;
   // reset output value to be 0
   output_val = 0;
   // shift values right in input buffer
